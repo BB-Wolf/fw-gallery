b52c35f2433c6af7bb648ca3dde4474f953ec117
mora <mora@localhost.localdomain>
2024-12-11 15:04:30 -0500

    work

--- src/components/pages/PersonalFolder.vue
+++ src/components/pages/PersonalFolder.vue
@@ -9,17 +9,58 @@
             <h2>Альбом {{ this.$route.params.name }}</h2>
         </div>
     </section>
+    <section id="single-image-edit">
+        <div class="profile-container d-flex g-20">
+            <div class="left-block">
+                <div>3335</div>
+                <img src="">
+            </div>
+            <div class="right-block">
+                <div class="group">
+                    <label>Название</label>
+                    <input type='text' value='123'>
+                </div>
+                <div class="group">
+                    <label>Описание</label>
+                    <textarea>233534534534543</textarea>
+                </div>
+                <div class="group">
+                    <label>Теги</label>
+                    <Multiselect v-model="tags" placeholder="Выберите теги" :filter-results="false" :min-chars="2"
+                :resolve-on-load="false" :mode="'multiple'" :delay="3" :close-on-select="true" :limit="10"
+                :searchable="true"></Multiselect>
+                </div>
+                <div class="group">
+                    <label>Раздел</label>
+                    <Multiselect v-model="tags" placeholder="Выберите теги" :filter-results="false" :min-chars="2"
+                :resolve-on-load="false" :mode="'multiple'"></Multiselect>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
 import axios from 'axios';
+import Multiselect from '@vueform/multiselect'
+
 export default {
+    components:{
+    Multiselect        
+    },
     data() {
         return {
             imagesList: []
         }
     },
     async created() {
-        const imagesList = await new axios.get('//img-fw.bb-wolf.site/console/get_gallery_picture.php?user=y', {});
+        const getImagesList = await new axios.get('//img-fw.bb-wolf.site/console/get_gallery_picture.php?user=y', {
+            headers: {
+                token: localStorage.get('token')
+            }
+        });
+        if (getImagesList.data) {
+            this.imagesList = getImagesList.data;
+        }
     }
 }
 </script>
@@ -37,5 +78,23 @@ h2 {
     padding: 20px;
     border-radius: 8px;
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5);
+}
+label{
+    color:white;
+    font-weight:700;
+    font-size:24px;
+}
+
+.d-flex{
+    display:flex;
+}
+.g-20{
+    gap:20px;
+}
+.left-block{
+    flex:1;
+}
+.right-block{
+    flex:3;
 }
 </style>
