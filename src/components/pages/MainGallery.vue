11bbe579d3f3edb12456dadcc0959d347aedbb16
Kirsanicate <arli-chan@yandex.ru>
2025-02-12 07:20:01 +0300

    fixes

--- src/components/pages/MainGallery.vue
+++ src/components/pages/MainGallery.vue
@@ -40,10 +40,8 @@ export default {
     <div>
         <Tags />
         <div class="gallery-switch">
-            <div class="switch-item" :class="[{ ['switch-item--active']: this.currentState == 'gallery' }]"
-                @click="switchGallery('gallery', $event)">Арты</div>
-            <div class="switch-item" :class="[{ ['switch-item--active']: this.currentState == 'comics' }]"
-                @click="switchGallery('comics', $event)">Комиксы</div>
+            <div class="switch-item" @click="switchGallery('gallery', $event)">Арты</div>
+            <div class="switch-item" @click="switchGallery('comics', $event)">Комиксы</div>
         </div>
         <div class="gallery-container" id="main-gallery">
             <GalleryBlock v-if="this.currentState == 'gallery'" needInfinty="y" galleryType="main"
