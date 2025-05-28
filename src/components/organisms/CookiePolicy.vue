4bc2c4b5acec7da81ae3d4ca4f4fdf651be97276
Kirsanicate <arli-chan@yandex.ru>
2025-05-29 02:52:22 +0300

    work

--- src/components/organisms/CookiePolicy.vue
+++ src/components/organisms/CookiePolicy.vue
@@ -1,22 +1,27 @@
 <template>
     <div class="cookie-policy">
-        <div class="policy-container">
+        <div class="policy-container" v-if="!isMobile">
             <h1>Политика использования файлов cookie</h1>
             <p>
                 Мы используем файлы cookie для улучшения вашего опыта на нашем сайте. Файлы cookie помогают нам
                 запомнить ваши предпочтения и настройки, а также анализировать трафик на сайте.
             </p>
-            <div class="hidden-mobile">
-                <p>
-                    Вы можете управлять настройками файлов cookie в вашем браузере. Если вы продолжаете использовать наш
-                    сайт,
-                    мы будем считать, что вы согласны с использованием файлов cookie.
-                </p>
-            </div>
+            <p>
+                Вы можете управлять настройками файлов cookie в вашем браузере. Если вы продолжаете использовать наш
+                сайт,
+                мы будем считать, что вы согласны с использованием файлов cookie.
+            </p>
             <p>
                 Для получения дополнительной информации о том, как мы используем файлы cookie, пожалуйста, ознакомьтесь
                 с
                 нашей <a href="/cookie-policy/">Политикой использования файлов Cookie</a>.
+            </p>
+        </div>
+        <div class="policy-container" v-if="isMobile">
+            <b>Политика использования файлов cookie</b>
+            <p>
+                Мы используем файлы cookie для улучшения вашего опыта на нашем сайте. Подробнее <a
+                    href="/cookie-policy/">Политикой использования файлов Cookie</a>.
             </p>
         </div>
         <div class="btn btn--success" @click="allowCookies">Принимаю</div>
@@ -57,6 +62,7 @@
 @media (max-width:760px) {
     .cookie-policy {
         flex-wrap: wrap;
+        flex-direction: column;
         ;
     }
 
@@ -66,9 +72,15 @@
 }
 </style>
 <script>
-import { allowedCookie } from '@main/state';
+import { allowedCookie, mobileDevice } from '@main/state';
+
 export default
     {
+        data() {
+            return {
+                isMobile: mobileDevice.isMobile,
+            }
+        },
         methods: {
             allowCookies() {
                 localStorage.setItem('allowcookie', 1);
